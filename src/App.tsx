480281eae42bfd207997a3dfef4c6ce016439777
Juan Feris <feris.juan@gmail.com>
2022-09-02 13:31:32 -0500

    feat(fe): add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import {MantineProvider, Text} from "@mantine/core";
+import {AppShell, Header, MantineProvider, Text} from "@mantine/core";
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <AppShell
+        padding="md"
+        header={
+          <Header height={60} p="xs">
+            <Text size="xl">Pokedex</Text>
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Text>Welcome to Pokedex!</Text>
+      </AppShell>
     </MantineProvider>
   );
 }
