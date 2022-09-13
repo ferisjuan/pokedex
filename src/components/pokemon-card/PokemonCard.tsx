c92b2c3ee92a305b3f66cc244529876ef0431543
Juan Feris <feris.juan@gmail.com>
2022-09-13 09:37:29 -0500

    feat(ui): refactor fetch pokemon

--- src/components/pokemon-card/PokemonCard.tsx
+++ src/components/pokemon-card/PokemonCard.tsx
@@ -1,38 +1,26 @@
 // @vendors
-import { Card, Group, Image, Table, Text } from "@mantine/core";
-import {useEffect, useMemo, useRef, useState} from "react";
+import { Card, Group, Image, Text } from "@mantine/core";
 
-// @interfaces
-import {Pokemon} from "../../interfaces";
+// @hooks
+import { useFetchPokemon } from './useFetchPokemon';
 
 // @lib
-import {fetcher, keygen} from "../../lib";
 import { PokemonStats } from '../pokemon-stats';
 
 // @types
-import {PokemonType} from "../pokemon-type";
+import { PokemonType } from "../pokemon-type";
 
 interface AppCardProps {
   url: string;
 }
 
-export const PokemonCard: React.FC<AppCardProps> = ({url}) => {
-  const [pokemon, setPokemon] = useState<Pokemon>();
-
-  useEffect(() => {
-    async function fetchPokemon() {
-      const _pokemon = await fetcher({url});
-
-      setPokemon(_pokemon);
-    }
-
-    fetchPokemon();
-  }, []);
+export const PokemonCard: React.FC<AppCardProps> = ({ url }) => {
+  const { pokemon } = useFetchPokemon(url)
 
   if (!pokemon) return null;
 
   return (
-    <Card p="lg" radius="md" shadow="sm" sx={{minHeight: "340px"}} withBorder>
+    <Card p="lg" radius="md" shadow="sm" sx={{ minHeight: "340px" }} withBorder>
       <Card.Section>
         <Image
           alt={pokemon.name}
@@ -40,15 +28,19 @@ export const PokemonCard: React.FC<AppCardProps> = ({url}) => {
           src={pokemon?.sprites.front_default}
         />
       </Card.Section>
+
       <Text weight={500}>{pokemon.name}</Text>
+
       <Group mb="xs" mt="md" position="left" spacing="xs">
-        {pokemon.types.map(({type}) => (
+        {pokemon.types.map(({ type }) => (
           <PokemonType key={type.name} type={type.name} />
         ))}
       </Group>
+
       <Group>
-          <PokemonStats stats={pokemon.stats}/>
+        <PokemonStats stats={pokemon.stats} />
       </Group>
+
       <Text size="sm" color="dimmed">
         pokemon stats, height width, attacks
       </Text>
