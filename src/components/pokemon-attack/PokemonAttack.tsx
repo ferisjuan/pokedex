636f51042548103b9f5b6d63af6558904096d709
Amir Salgado <gigas@192.168.1.7>
2022-10-07 09:21:33 -0500

    Create a carousel to show  pokemon moves

--- src/components/pokemon-attack/PokemonAttack.tsx
+++ src/components/pokemon-attack/PokemonAttack.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Move } from '../../interfaces';
+import React from "react";
+import { Carousel } from "@mantine/carousel";
+import { Move } from "../../interfaces";
 
 interface PokemonAttackProps {
-  pokemonMoves: Move[]
+  pokemonMoves: Move[];
 }
 
-export const PokemonAttack: React.FC<PokemonAttackProps> = ({ pokemonMoves }) => (
-  <div>
-    {
-      pokemonMoves.map((pokemonMove) => (
-        <h2>
-          {pokemonMove.move.name}
-        </h2>
-      ))
-    }
-  </div>
-)
+export const PokemonAttack: React.FC<PokemonAttackProps> = ({
+  pokemonMoves,
+}) => (
+  <Carousel
+  sx={{ maxWidth: 320 }}
+  mx="auto"
+  withIndicators
+  height={200}
+  dragFree
+  slideGap="md"
+  align="start"
+  >
+    {pokemonMoves.map((pokemonMove) => (
+        <Carousel.Slide>{pokemonMove.move.name}</Carousel.Slide>
+    ))}
+  </Carousel>
+);
